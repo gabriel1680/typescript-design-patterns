1f7bb1fff1736ae80339a2048b560ab5fab76c1e
Gabriel P. Lopes <gabriel.lopes1680@gmail.com>
2022-12-31 15:05:46 -0300

    docs(creational): add note

--- src/creational/builder/descriptoin.ts
+++ src/creational/builder/descriptoin.ts
@@ -1,5 +1,7 @@
 /**
  * The builder pattern is a creational pattern that simplify the creation of an instance of a class
  * that have a complex constructor by adding descriptive methods and most times abstracting the creation of
- * another objects that composes the subject class
+ * another objects that composes the subject class.
+ *
+ * NOTE: The implementation example was made with a instance class builder, but it can made with a static class as well (what simplifies the usage)
  */
